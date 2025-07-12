122456474e53328adf69583eab0bffefdca0220b
Alan <alan.melgar.f@gmail.com>
2025-07-12 19:41:56 -0400

    retoques

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { HeroSection } from '@/components/HeroSection';
-import { CarouselSection } from '@/components/SideCarousel';
 
 export default function Home() {
   return (
     <div className="relative">
       <HeroSection />
-      <CarouselSection />
     </div>
   );
 }
