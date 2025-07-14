63b154e67f29eb4363e48226c275ef877a82554f
Job Natanael Contreras Garzon <143237173+Job-Natanael-Contreras-Garzon@users.noreply.github.com>
2025-07-14 01:07:26 -0400

    update contain

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ export function Logo() {
         alt="El Orquideario Logo"
         width={32}
         height={32}
-        className="object-cover md:w-8 md:h-8 lg:w-10 lg:h-10 opacity-90"
+        className="object-contain md:w-8 md:h-8 lg:w-10 lg:h-10 opacity-90"
       />
       <span className="font-headline text-lg md:text-2xl lg:text-3xl font-bold tracking-tight">
         El Orquideario
